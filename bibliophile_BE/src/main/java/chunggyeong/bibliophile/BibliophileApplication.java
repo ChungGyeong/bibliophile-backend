42e8e336eb1e33c6061922bf2aa31bc269602089
이건희 <022686s@gmail.com>
2024-09-09 15:04:03 +0900

    init : backend server upload

---
+++ bibliophile_BE/src/main/java/chunggyeong/bibliophile/BibliophileApplication.java
@@ -0,0 +1,13 @@
+package chunggyeong.bibliophile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliophileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliophileApplication.class, args);
+	}
+
+}
---
+++ bibliophile_BE/src/test/java/chunggyeong/bibliophile/BibliophileApplicationTests.java
@@ -0,0 +1,13 @@
+package chunggyeong.bibliophile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliophileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
