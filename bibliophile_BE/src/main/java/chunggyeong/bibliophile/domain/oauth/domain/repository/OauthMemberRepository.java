715baf2bb9771a0bd4fdd4a123885f23cd090b53
이건희 <022686s@gmail.com>
2024-09-13 09:06:45 +0900

    feat: oauthMember 개발 - 회원 탈퇴

--- bibliophile_BE/src/main/java/chunggyeong/bibliophile/domain/oauth/domain/repository/OauthMemberRepository.java
+++ bibliophile_BE/src/main/java/chunggyeong/bibliophile/domain/oauth/domain/repository/OauthMemberRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 public interface OauthMemberRepository extends JpaRepository<OauthMember, Long> {
 
     Optional<OauthMember> findByOauthServerTypeAndEmail(OauthServerType oauthServerType, String email);
+
+    void deleteByEmailAndOauthServerType(String email, OauthServerType oauthServerType);
 }
