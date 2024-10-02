f636b608a608b1acb1e17a0351ebf29f19970894
이건희 <022686s@gmail.com>
2024-10-02 17:23:31 +0900

    feat: 관심사 설정

--- bibliophile_BE/src/main/java/chunggyeong/bibliophile/domain/interest/domain/Classification.java
+++ bibliophile_BE/src/main/java/chunggyeong/bibliophile/domain/interest/domain/Classification.java
@@ -6,15 +6,15 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Classification {
-    GENERAL_WORKS("총류"),
-    PHILOSOPHY("철학"),
-    RELIGION("종교"),
-    SOCIAL_SCIENCES("사회과학"),
-    NATURAL_SCIENCES("언어"),
-    TECHNOLOGY("기술과학"),
+    ECONOMICS("경제"),
+    COMICS("만화"),
+    SOCIETY("사회"),
+    LOVE("사랑"),
+    FICTION("소설"),
+    TRAVEL("여행"),
+    IT_SCIENCE("IT/과학"),
     ARTS("예술"),
     LANGUAGE("언어"),
-    LITERATURE("문학"),
     HISTORY("역사");
 
     private final String value;
