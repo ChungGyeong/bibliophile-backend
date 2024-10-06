2cd6395b07591cdf5baa632673b073c9295fa552
chpark6851 <chpark6851@koreatech.ac.kr>
2024-10-06 15:54:36 +0900

    fix : Security Config deprecated 코드 최신화

--- bibliophile_BE/src/main/java/chunggyeong/bibliophile/global/config/SecurityConfig.java
+++ bibliophile_BE/src/main/java/chunggyeong/bibliophile/global/config/SecurityConfig.java
@@ -5,8 +5,12 @@ import chunggyeong.bibliophile.global.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -23,7 +27,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.formLogin().disable().cors().and().csrf().disable();
+        http
+                .httpBasic(HttpBasicConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .rememberMe(AbstractHttpConfigurer::disable)
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http
                 .authorizeHttpRequests(authorize -> authorize
