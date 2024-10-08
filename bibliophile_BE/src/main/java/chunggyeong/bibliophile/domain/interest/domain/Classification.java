8f592400940b559912ad41d5aceced945881d7c0
이건희 <022686s@gmail.com>
2024-10-08 14:09:23 +0900

    fix: 관심사 수정

--- bibliophile_BE/src/main/java/chunggyeong/bibliophile/domain/interest/domain/Classification.java
+++ bibliophile_BE/src/main/java/chunggyeong/bibliophile/domain/interest/domain/Classification.java
@@ -6,13 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Classification {
-    ECONOMICS("경제"),
-    COMICS("만화"),
     SOCIETY("사회"),
-    LOVE("사랑"),
     FICTION("소설"),
-    TRAVEL("여행"),
-    IT_SCIENCE("IT/과학"),
+    SCIENCE("과학"),
     ARTS("예술"),
     LANGUAGE("언어"),
     HISTORY("역사");
